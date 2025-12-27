ef220de4768aa80f04f00d06d1d2a0a192c2b161
hunter-devil2057 <87434771+hunter-devil2057@users.noreply.github.com>
2025-12-27 08:31:09 +0545

    Python Basics

---
+++ 01.Python_Basics/Introduction_to_Python/Day1.py
@@ -0,0 +1,9 @@
+# the print() function is used to display the output to the console. 
+print("Hello World")
+
+# Assigning a Variable with value 10 and 5, 
+a = 10
+b = 5
+# Printing the sum of two variables
+print("a + b = " + str(a + b))
+print(f"a + b ", (a + b))

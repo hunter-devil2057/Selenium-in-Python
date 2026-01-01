e5cc1dc3a9704386cd32cacf54b204cb1e3ee022
hunter-devil2057 <87434771+hunter-devil2057@users.noreply.github.com>
2026-01-01 17:49:09 +0545

    Various Functions used in Python Dictionaries

---
+++ 02.Data-Types-in-Python/04.Dictionaries/Functions-used-in-Dictionaries.py
@@ -0,0 +1,8 @@
+my_dic = {'id': 'e156', 'name': 'Allen', 'age': 34, 'salary': 15000}
+print(my_dic)
+
+# copy(), clear(), pop(), get(), items(), keys(), values(), update(), len()
+data = {'post': 'Engineer', 'city': 'California', 'age': 42}
+my_dic.update(data)
+print(my_dic)
+print(len(my_dic))

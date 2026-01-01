39f41d5fc882776108818083488399b09ca0d1ec
hunter-devil2057 <87434771+hunter-devil2057@users.noreply.github.com>
2026-01-01 17:39:35 +0545

    Simple Program for Function in Python

---
+++ 01.Python_Basics/Functions/function.py
@@ -0,0 +1,12 @@
+# Functions in Python
+
+def message():
+    print("Hello, I am Function")
+
+def addition(a, b):
+    print("The sum is:", a + b)
+
+# Calling a Function 
+message()
+addition(10, 20)
+addition(5, 15)

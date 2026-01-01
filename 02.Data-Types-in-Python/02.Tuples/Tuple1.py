0dc7a2476dbc0e28818fc22a6416e6d8d684969d
hunter-devil2057 <87434771+hunter-devil2057@users.noreply.github.com>
2026-01-01 17:44:57 +0545

    Tuples in Python

---
+++ 02.Data-Types-in-Python/02.Tuples/Tuple1.py
@@ -0,0 +1,3 @@
+my_tuple = (10, 34, 56, 3, 10, 34, 10, "John", "Japan", True)
+print(my_tuple)
+print(id(my_tuple))
---
+++ 02.Data-Types-in-Python/02.Tuples/Tuple2.py
@@ -0,0 +1,8 @@
+my_tuple = (10, 34, 56, 3, 10, 34, 10, "John", "Japan", True)
+print(my_tuple)
+print(id(my_tuple))
+
+tuple_b = (15, 26, 49, 51, 86, 64, 96, 38, 26, 26, 102, 5)
+my_tuple = my_tuple + tuple_b
+print(my_tuple)
+print(id(my_tuple))

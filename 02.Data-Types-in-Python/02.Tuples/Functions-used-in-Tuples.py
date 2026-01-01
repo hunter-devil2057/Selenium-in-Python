351ff2a6feb4f92b38ec0af4eaf921504210e972
hunter-devil2057 <87434771+hunter-devil2057@users.noreply.github.com>
2026-01-01 17:45:11 +0545

    Functions used in Python Tuples

---
+++ 02.Data-Types-in-Python/02.Tuples/Functions-used-in-Tuples.py
@@ -0,0 +1,8 @@
+tuple_b = (15, 26, 49, 51, 86, 64, 96, 38, 26, 26, 102, 5)
+
+# count(), index(), max(), min(), len()
+print(tuple_b.count(26))
+print(tuple_b.index(26))
+print(max(tuple_b))
+print(min(tuple_b))
+print(len(tuple_b))

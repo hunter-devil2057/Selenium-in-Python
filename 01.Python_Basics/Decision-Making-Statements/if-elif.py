f5849025907b43663ac98ee4439857f1d6a6cceb
hunter-devil2057 <87434771+hunter-devil2057@users.noreply.github.com>
2026-01-01 17:37:56 +0545

    If Else Statement (Decision Making)

---
+++ 01.Python_Basics/Decision-Making-Statements/if-elif.py
@@ -0,0 +1,15 @@
+a = 100
+
+# if-elif statements
+if a == 20:
+    print("value of a is 20")
+elif a == 30:
+    print("value of a is 30")
+elif a == 40:
+    print("value of a is 40")
+elif a == 50:
+    print("value of a is 50")
+else:
+    print("a is out of given scope")
+
+print("Out of if statement block")

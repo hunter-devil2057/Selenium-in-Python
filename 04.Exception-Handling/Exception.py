65087bce9d3506445e386012ba84a5f53a08b17c
hunter-devil2057 <87434771+hunter-devil2057@users.noreply.github.com>
2026-01-01 17:47:25 +0545

    Exception Condition in Python

---
+++ 04.Exception-Handling/Exception.py
@@ -0,0 +1,10 @@
+print("Hi, I am Jack")
+print("Hi, I am from Tokyo")
+
+a = int(input("Enter the value of a : "))
+b = int(input("Enter the value of b : "))
+
+def addition():
+    print("Division of a and b is ", a/b)
+
+addition()
